d5c57ae35086e8e9de20a4baad4476063d3edaed
Pyae Shan <72793369+Happycloud250@users.noreply.github.com>
2022-03-04 23:35:20 +0630

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
-const dice1 = document.querySelector(".dice1 img");
-const dice2 = document.querySelector(".dice2 img");
-const dice1Heading = document.querySelector(".dice1Heading");
-const dice2Heading = document.querySelector(".dice2Heading");
+const dice1Img = document.querySelector(".dice1 img");
+const dice2Img = document.querySelector(".dice2 img");
+const dice1Heading = document.querySelector(".dice1 h2");
+const dice2Heading = document.querySelector(".dice2 h2");
 
 const play = () => {
   const randomNumber1 = Math.floor(Math.random() * 6) + 1;
   const randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-  dice1.setAttribute("src", `${randomNumber1}.jpg`);
-  dice2.setAttribute("src", `${randomNumber2}.jpg`);
+  dice1Img.setAttribute("src", `${randomNumber1}.jpg`);
+
+  dice2Img.setAttribute("src", `${randomNumber2}.jpg`);
   if (randomNumber1 > randomNumber2) {
     dice1Heading.innerHTML = "Player 1 Wins!!!";
     dice2Heading.innerHTML = "Player 2 Lose!!!";
     dice1Heading.style.color = "red";
-    dice2Heading.style.color = "rgba(128, 128, 128, 0.719)";
+    dice2Heading.style.color = "rgb(170, 27, 236)";
 
     dice1Heading.style.color;
   } else if (randomNumber1 < randomNumber2) {
     dice1Heading.innerHTML = "Player 1 Lose!!!";
     dice2Heading.innerHTML = "Player 2 Wins!!!";
-    dice1Heading.style.color = "rgba(128, 128, 128, 0.719)";
+    dice1Heading.style.color = "rgb(170, 27, 236)";
     dice2Heading.style.color = "red";
   } else {
     dice1Heading.innerHTML = "Draw!!!";
